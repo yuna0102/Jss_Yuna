339acabc53fd936716a1a68f66fe0551148198eb
unknown <yuna0102>
2020-11-15 18:03:43 +0900

    [success] login

--- JssProject/accounts/urls.py
+++ JssProject/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import signup
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('signup/', signup, name="signup"),
+    path('login/', LoginView.as_view(), name="login"),
 ]
