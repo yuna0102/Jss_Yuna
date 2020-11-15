eec744553e0e858fa7761c1b2c1c90c3f6be8d59
unknown <yuna0102>
2020-11-15 17:34:02 +0900

    [add] account user

--- JssProject/JssProject/urls.py
+++ JssProject/JssProject/urls.py
@@ -11,18 +11,15 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('앱이름.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main.views import index, create, detail, delete, update
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
-    path('create/', create, name="create"),
-    path('detail/<int:jss_id>', detail, name="detail"),
-    path('delete/<int:jss_id>', delete, name="delete"),
-    path('update/<int:jss_id>', update, name="update"),
+    path('', include('main.urls')),
+    path('', include('accounts.urls')),
 ]
--- JssProject/accounts/views.py
+++ JssProject/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def signup(request):
+    regi_form = UserCreationForm()
+    if request.method == "POST":
+        filled_form = UserCreationForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            return redirect('index')
+    return render(request, 'signup.html', {'regi_form' : regi_form})
