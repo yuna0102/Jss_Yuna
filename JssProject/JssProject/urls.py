737837e049f9b5c1bb038b596b3619cd44d1f7f8
unknown <yuna0102>
2020-11-15 16:49:59 +0900

    [finish] pk up/del

--- JssProject/JssProject/urls.py
+++ JssProject/JssProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import index, create, detail
+from main.views import index, create, detail, delete, update
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -23,4 +23,6 @@ urlpatterns = [
     path('', index, name="index"),
     path('create/', create, name="create"),
     path('detail/<int:jss_id>', detail, name="detail"),
+    path('delete/<int:jss_id>', delete, name="delete"),
+    path('update/<int:jss_id>', update, name="update"),
 ]
--- JssProject/main/views.py
+++ JssProject/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import JssForm
 from .models import Jasoseol
+from django.http import Http404
 # Create your views here.
 def index(request):
     all_jss = Jasoseol.objects.all()
@@ -20,5 +21,25 @@ def create(request):
     return render(request, 'create.html', {'jss_form' : jss_form})
 
 def detail(request, jss_id):
-    my_jss = Jasoseol.objects.get(pk=jss_id)
+    # try:
+    #     my_jss = Jasoseol.objects.get(pk=jss_id)
+    # except:
+    #     raise Http404
+    my_jss = get_object_or_404(Jasoseol, pk=jss_id)
     return render(request, 'detail.html', {'my_jss' : my_jss})
+
+def delete(reqeust, jss_id):
+    my_jss =Jasoseol.objects.get(pk=jss_id)
+    my_jss.delete()
+    return redirect('index')
+
+def update(request, jss_id):
+    my_jss =Jasoseol.objects.get(pk=jss_id)
+    #modelform을 사용하겠다고 선언
+    jss_form = JssForm(instance=my_jss)
+    if request.method == "POST":
+        updated_form = JssForm(request.POST, instance=my_jss)
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirect('index')
+    return render(request, 'create.html', {'jss_form' : jss_form})
